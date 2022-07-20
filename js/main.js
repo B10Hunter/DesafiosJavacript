b705c8f6ab815740c5e1ac4df5e68db5c9c4b289
Lucas Albornoz <flipskate@outlook.es>
2022-07-20 12:43:45 -0300

    new

---
+++ js/main.js
@@ -0,0 +1,33 @@
+class User {
+    constructor (username,email,password){
+        this.username = username
+        this.email = email
+        this.password = password
+        
+    }
+    
+}
+
+const persona  = []
+
+const form = document.getElementById('idform')
+
+form.addEventListener('submit', (event) => {
+       event.preventDefault()
+      
+       let username = document.getElementById('idNombre').value
+       let email = document.getElementById('idEmail').value
+       let password = document.getElementById('idContra').value
+
+       const user = new User (username, email ,password)
+       persona.push (user)
+       console.log(persona)
+})
+ const imagen = document.getElementById ('img')
+
+ imagen.innerHTML += `
+    <div class ="centrado">
+     <img  src="img/1f60a.png" alt="img">
+     </div>
+ `
+   
