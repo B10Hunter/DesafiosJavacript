a181ca973c5a8e2c6b5b444ed31ce4ca6a771c63
Lucas Albornoz <flipskate@outlook.es>
2022-08-15 01:34:10 -0300

    problema con la img

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ personaje.addEventListener('submit', (e) => {
             <div class="h-100 m-auto p-5 text-bg-dark rounded-3">
                 <p class="  h2 border-4 text-center border-bottom border-white h2">Nombre ${item.name} </p>
                 <div class="text-center">
-                 <img class="rounded imagen" src="${item.thumbnail.path}/portrait_uncanny.${item.thumbnail.extension}" alt="" srcset="">
+                 <img class="rounded imagen" src="${item.thumbnail.path}/portrait_uncanny.${item.thumbnail.extension}" alt="img" >
                 </div>
             </div>
          </div>`
