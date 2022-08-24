0a22bdee3b3fba8da4b5bfd627975369c4112a9d
Lucas Albornoz <flipskate@outlook.es>
2022-08-23 23:49:25 -0300

    gh-pages added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import personas from './data/personas.json';
+const App = ()=> {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>desafio1</p>
+        <hr></hr>
+        {personas.map( persona => (
+          <div> 
+            <p> Nombre:{persona.nombre}</p>
+            <p> Edad:{persona.edad}</p>
+            <hr></hr>
+          </div>
+        ))
+        }
     </div>
   );
 }
 
 export default App;
+ 
